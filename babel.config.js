593604f05450895abcf0fb22b712383563c2cc85
MmeliSyanda <22355803@live.mut.ac.za>
2026-05-21 10:41:32 +0200

    Fix APK build: OTEL babel plugin, Android permissions, push notification packages

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,30 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      function replaceVariableDynamicImports({ types: t }) {
+        return {
+          visitor: {
+            ImportExpression(path) {
+              const source = path.node.source;
+              if (
+                source.type !== "StringLiteral" &&
+                source.type !== "TemplateLiteral"
+              ) {
+                path.replaceWith(
+                  t.callExpression(
+                    t.memberExpression(
+                      t.identifier("Promise"),
+                      t.identifier("resolve")
+                    ),
+                    [t.objectExpression([])]
+                  )
+                );
+              }
+            },
+          },
+        };
+      },
+    ],
   };
 };
